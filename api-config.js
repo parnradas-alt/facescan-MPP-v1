55ff74e8821da2ca9a8b1117dd18f4b90d7f5d4a
parnradas-alt <parnradas@gmail.com>
2026-04-08 22:59:15 +0700

    Update GAS API URL in api-config.js

--- api-config.js
+++ api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby06NcqZZFzqJdPFXQ40duY55gwxxixofv4lH1y3BIDzcK7m1A-yN2uMYO6CzYv8XN3/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxAO9vRwOnh0AroR37KkxeP9cRXt5y9K8CVqmVaulr4dsg254wgTnVsGYduGtSzecaz/exec';
