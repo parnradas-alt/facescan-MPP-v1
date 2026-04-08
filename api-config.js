af3e800bcf83189ca705074cb95c78167411ddb6
parnradas-alt <parnradas@gmail.com>
2026-04-09 00:14:53 +0700

    Delete api-config.js

--- api-config.js
+++
@@ -1,5 +0,0 @@
-// ============================================================
-//  ตั้งค่า Google Apps Script Web App URL
-//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
-// ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxAO9vRwOnh0AroR37KkxeP9cRXt5y9K8CVqmVaulr4dsg254wgTnVsGYduGtSzecaz/exec';
